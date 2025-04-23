8ba947d8c60f8f6e880a3fe7ebc130af11727c6e
Souvik Roy <dev.souvikroy92@gmail.com>
2025-04-23 17:23:58 +0530

    cors added

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const Books = require("./models/books.models");
 
 initializeDatabase();
 
+// CORS
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 // const jsonData = fs.readFileSync("books.json", "utf-8");
 // const booksData = JSON.parse(jsonData);
 
